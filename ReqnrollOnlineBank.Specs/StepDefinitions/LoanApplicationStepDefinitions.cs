737f736f20859ef9b72708b2427bb3284feebec8
Bas Dijkstra <bas@ontestautomation.com>
2025-01-16 15:03:55 +0100

    Add code for chapter 5

---
+++ ReqnrollOnlineBank.Specs/StepDefinitions/LoanApplicationStepDefinitions.cs
@@ -0,0 +1,34 @@
+using System;
+using Reqnroll;
+
+namespace ReqnrollOnlineBank.Specs.StepDefinitions
+{
+    [Binding]
+    public class LoanApplicationStepDefinitions
+    {
+        [Given("John is an active ParaBank customer")]
+        public void GivenJohnIsAnActiveParaBankCustomer()
+        {
+        }
+
+        [When("they apply for a {int} dollar loan")]
+        public void WhenTheyApplyForADollarLoan(int p0)
+        {
+        }
+
+        [Then("the loan application is approved")]
+        public void ThenTheLoanApplicationIsApproved()
+        {
+        }
+
+        [Then("the loan application is denied")]
+        public void ThenTheLoanApplicationIsDenied()
+        {
+        }
+
+        [When("their monthly income is {int}")]
+        public void WhenTheirMonthlyIncomeIs(int p0)
+        {
+        }
+    }
+}
