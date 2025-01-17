dfeb4333d28e9d3ac54d49dd965f0d1c8361df6e
Bas Dijkstra <bas@ontestautomation.com>
2025-01-17 10:34:51 +0100

    Add code for chapter 7

---
+++ ReqnrollOnlineBank.Specs/Models/LoanApplication.cs
@@ -0,0 +1,9 @@
+﻿namespace ReqnrollOnlineBank.Specs.Models
+{
+    public class LoanApplication
+    {
+        public string Applicant { get; set; } = string.Empty;
+        public int Amount { get; set; } = 0;
+        public string Status { get; set; } = "New";
+    }
+}
